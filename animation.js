85912dd69123ecad3ef70886a35d33a0abc0b239
shivc8658 <shiv.kumar@tasolglobal.com>
2024-08-04 13:51:19 +0530

    hero section anim

---
+++ animation.js
@@ -0,0 +1,65 @@
+// hero-qx-img
+
+gsap.fromTo(
+  ".hero-qx-img",
+  {
+    opacity: 0,
+    y: 500,
+    ease: "power4.out",
+  },
+  {
+    opacity: 1,
+    duration: 1,
+    y: 0,
+    ease: "power4.out",
+  }
+);
+// quality-text-animation
+gsap.fromTo(
+  ".quality-text-animation",
+  {
+    opacity: 0,
+    y: 100,
+  },
+  {
+    opacity: 1,
+    duration: 1,
+    delay: 0.2,
+    y: 0,
+    ease: "power4.out",
+  }
+);
+
+// HERO OVEL IMAGE ANIMATION
+
+gsap.fromTo(
+  ".hero-ovel-image",
+  {
+    opacity: 0,
+    y: 100,
+  },
+  {
+    opacity: 1,
+    duration: 1,
+    delay: 0.2,
+    y: 0,
+    ease: "power4.out",
+  }
+);
+// explore-btn-anim
+
+gsap.fromTo(
+  ".explore-btn-anim",
+  {
+    opacity: 0,
+    y: 100,
+    ease: "power4.out",
+  },
+  {
+    opacity: 1,
+    duration: 1,
+    delay: 0.2,
+    y: 0,
+    ease: "power4.out",
+  }
+);
